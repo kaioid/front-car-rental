53c5da1b34874ce205eb1e04f2b9c8166d187db8
Kaio <kaioigordinizz@hotmail.com>
2022-12-28 15:13:51 -0300

    ajuste url

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -6,7 +6,7 @@ import { Observable } from "rxjs";
     providedIn: 'root'
 })
 export class CarService {
-    private baseUrl = 'http://localhost:8080/vehicles';
+    private baseUrl = 'http://localhost:8080/api/vehicles';
 
     constructor(private http: HttpClient) { }
 
